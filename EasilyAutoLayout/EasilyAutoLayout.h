834bff326f0b86d9eeb7ebc5c103476d7fe13b35
Rintaro Kawagishi <rintaro.kawagishi.developer@gmail.com>
2020-04-06 21:37:00 +0900

    Initial Commit

---
+++ EasilyAutoLayout/EasilyAutoLayout.h
@@ -0,0 +1,19 @@
+//
+//  EasilyAutoLayout.h
+//  EasilyAutoLayout
+//
+//  Created by Rintaro Kawagishi on 06/04/2020.
+//  Copyright © 2020 Rintaro Kawagishi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EasilyAutoLayout.
+FOUNDATION_EXPORT double EasilyAutoLayoutVersionNumber;
+
+//! Project version string for EasilyAutoLayout.
+FOUNDATION_EXPORT const unsigned char EasilyAutoLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasilyAutoLayout/PublicHeader.h>
+
+
